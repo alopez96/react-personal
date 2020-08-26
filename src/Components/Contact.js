7cad8288246909551d73902762a5e18bd48164c3
Arturo Lopez <arturolopez.developer@gmail.com>
2020-08-25 17:46:47 -0700

    updated contact section with form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,7 +5,7 @@ import NavBar from './NavBar'
 import { SMALL } from '../ScreenSizes'
 
 const Rect = styled.div`
-    width: 30vw;
+    width: 40vw;
     height: 100vh;
     display: flex;
     margin-top: 10rem;
@@ -36,6 +36,27 @@ const Center = styled.div`
     align-items: center;
 `
 
+const Button = styled.div`
+    width: 4rem;
+    height: 4rem;
+    // border-radius: 50%;
+    padding: 2rem 4rem 2rem 4rem;
+    font-size: 1.4rem;
+    background-image: ${props => `linear-gradient(45deg, ${props.color.from} 0%, ${props.color.to} 100%)`};
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: box-shadow .5s, transform .5s;
+    &:hover {
+        transform: scale(1.1);
+        box-shadow: 0 3px 6px rgba(0,0,0,.16);
+    }
+`
+
 const openLink = (url) => () => {
     if(url.charAt(0) !== "h"){
         window.location = `mailto:${url}`;
@@ -52,10 +73,10 @@ const Contact = (props) => {
             <Rect>
                 <Content>
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                    <legend className="f1 fw6 ph0 mh0">Contact Us</legend>
+                    <legend className="f1 fw6 ph0 mh0">Contact</legend>
                     <div className="mt3">
                     <label className="db fw6 lh-copy f6" htmlFor="name">Name *</label>
-                    <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                    <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-80" 
                     type="text"
                     name="name"
                     id="name"
@@ -64,7 +85,7 @@ const Contact = (props) => {
                     </div>
                     <div className="mt3">
                     <label className="db fw6 lh-copy f6" htmlFor="email-address">Your Email *</label>
-                    <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                    <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-80" 
                     type="email" 
                     name="email-address"  
                     id="email-address"
@@ -73,7 +94,7 @@ const Contact = (props) => {
                     </div>
                     <div className="mv3">
                     <label className="db fw6 lh-copy f6" htmlFor="text">Your Message *</label>
-                    <textarea className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 h-400" 
+                    <textarea className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-80" rows={4}
                     type="text" 
                     name="text"  
                     id="text"
@@ -88,6 +109,7 @@ const Contact = (props) => {
                         className="b ph3 pv2 ba b--black bg-transparent pointer f6 dib">Send</p>
                     </div>
                     } */}
+                    <Button color={{from: "#53b8c5", to: "#2E151B"}}>Send</Button>
                     
                 </fieldset>
                 </Content>
