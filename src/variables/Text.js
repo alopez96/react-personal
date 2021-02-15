2dc36eacb0089731b7673591878af7a60db555a1
Arturo Lopez <arturolopez.developer@gmail.com>
2021-02-15 08:55:31 -0800

    removed @ on home text

--- src/variables/Text.js
+++ src/variables/Text.js
@@ -1,6 +1,6 @@
 const about = {
     header: 'Hi, I’m Arturo Lopez.',
-    sub_header: 'I am a Software Engineer, Tech analyst @ Accenture. 👨🏾‍💻\n I also help small business level up their website.',
+    sub_header: 'I am a Software Engineer, Tech analyst at Accenture. 👨🏾‍💻\n I also help small business level up their website.',
     contact_header: 'I am passionate about combining my software skills with design, to give users a clean and friendly user experience.',
     contact_sub_head: 'After recieving my Bachelors\' from UC Santa Cruz, I did some labor work in construction while I applied for jobs in my field of interest.' +
         'In November, 2019 I started working at Accenture, where I joined the Analytics Team.'
